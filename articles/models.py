0092ff28a56c1449d67075d801298a08662bbf84
jzingh@ucdavis.edu <jzingh@ucdavis.edu>
2020-10-28 00:32:57 -0700

    Upto part 20

--- articles/models.py
+++ articles/models.py
@@ -6,5 +6,13 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    # thumbnail
+    thumb = models.ImageField(default="default.png", blank=True)
     # author
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50] + '...'
+
+
